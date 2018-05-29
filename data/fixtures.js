c8425bfaa62dc7ed04a2fa4e77d9b5f3e0354e22
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-05-29 09:54:36 -0600

    initial commit

---
+++ data/fixtures.js
@@ -0,0 +1,22 @@
+module.exports = {
+  cake: [{
+    id: 1,
+    type: 'Piped Cake Doughnuts'
+  },
+  {
+    id: 2,
+    type: 'Rolled-Out Cake Doughnuts'
+  },
+  {
+    id: 3,
+    type: 'Crullers'
+  },
+  {
+    id: 4,
+    type: 'Cider doughnuts'
+  },
+  {
+    id: 5,
+    type: 'Old-Fashioned Doughnuts'
+  }
+  ]}
